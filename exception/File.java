3610f772ac81452a351e3691ab11b8c568b76b0d
charleslukes <charleslukes28@gmail.com>
2020-02-10 02:26:16 +0100

    create file java

---
+++ exception/File.java
@@ -0,0 +1,30 @@
+
+package exception;
+
+/**
+ * File
+ */
+public class File {
+
+    public static void main(String[] args) {
+        java.io.File file = new java.io.File("scores.txt");
+
+        if (file.exists()) {
+            System.out.println("File already exits");
+            System.exit(1);
+        }
+
+        try {
+            java.io.PrintWriter output = new java.io.PrintWriter(file);
+            output.print("Hello Charles ");
+            output.print("\nThis is a printer app ");
+            output.print("I'm here for you!");
+
+            output.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getMessage();
+        }
+    }
+}
