78ea39dd6bc7e6bb30e2ed59553f8d42f8649610
charleslukes <charleslukes28@gmail.com>
2020-02-10 02:26:34 +0100

    handle input mismatch exceptions

---
+++ exception/InputMisMatch.java
@@ -0,0 +1,34 @@
+/**
+ * (InputMismatchException) Write a program that prompts the user 
+ * to read two integers and displays their sum. Your program should 
+ * prompt the user to read the number again if the input is incorrect
+ */
+
+package exception;
+
+import java.util.*;
+
+/**
+ * InputMisMatch
+ */
+public class InputMisMatch {
+
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers");
+
+        // using a try resource to close resources.
+        try (Scanner input = new Scanner(System.in)) {
+
+            int firstNum = input.nextInt();
+            int secondNum = input.nextInt();
+
+            // displays result when user input is correct.
+            System.out.println(firstNum + secondNum);
+
+        } catch (InputMismatchException e) {
+            // displays this when user input is not a number.
+            System.out.println("Input must be a number ");
+        }
+
+    }
+}
