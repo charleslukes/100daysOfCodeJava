2e388f9ea426de262033a47ec3305dd847c2fcfe
charleslukes <charleslukes28@gmail.com>
2020-02-11 02:25:01 +0100

    create file java

---
+++ exception/CreateFile.java
@@ -0,0 +1,38 @@
+/**
+ * (Create a directory) Write a program that prompts the user to enter a
+ * directory name and creates a directory using the File’s mkdirs method. The
+ * program displays the message “Directory created successfully” if a directory
+ * is created or “Directory already exists” if the directory already exists.
+ */
+
+package exception;
+
+/**
+ * CreateFile
+ */
+
+import java.util.*;
+
+public class CreateFile {
+
+    public static void main(String[] args) {
+
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter a directory name: ");
+
+            String directoryName = input.next();
+            java.io.File file = new java.io.File(directoryName);
+
+            if (file.mkdir()) {
+                System.out.println("Directory created successfully");
+            } else {
+                System.out.println("Directory already exits");
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Input must be a valid string");
+        }
+
+    }
+
+}
