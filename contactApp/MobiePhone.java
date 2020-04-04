6975facefa9ab1dd002cb94a4911ac4657835402
Charles Chiakwa <cchiakwa@seamfix.com>
2020-04-04 16:19:08 +0100

    add main mobile phone class

---
+++ contactApp/MobiePhone.java
@@ -0,0 +1,19 @@
+/**
+ *  create a program that implements a simple phone with capabilities
+ *  Store, modify, remove and query contact name.
+ */
+
+package contactApp;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MobiePhone {
+    private static Scanner scan = new Scanner(System.in);
+    private static ArrayList<Contact> contact = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+    }
+}
